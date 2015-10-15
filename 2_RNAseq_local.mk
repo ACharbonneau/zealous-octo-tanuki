c2e3a0215e79e92540249914c5b316f86852de3b
Amanda Charbonneau <charbo24@msu.edu>
2015-10-15 14:37:32 -0400

    Finally got it all working I couldnt get Make to take the union of two rules for the samfile targets. So I just make the genome build a conditional in the sam file rule. Tried forever to get the sort and index statements seperated but couldn't. Something about the naming conventions breaks.

--- 2_RNAseq_local.mk
+++ 2_RNAseq_local.mk
@@ -10,7 +10,7 @@ Aligned_reads.txt : ChloroSat_SamFiles/${kmer}.chloro.sorted Sativus_SamFiles/${
 
 # Convert New Files
 
-ChloroSat_SamFiles/${kmer}.chloro.sorted: ChloroSat_SamFiles/${kmer}.chloro.bam
+ChloroSat_SamFiles/${kmer}.chloro.sorted : ChloroSat_SamFiles/${kmer}.chloro.bam
 	samtools sort $^ $@
 	samtools index $@.bam
 
@@ -24,7 +24,7 @@ Sativus_SamFiles/${kmer}.sat.sorted: Sativus_SamFiles/${kmer}.sat.bam
 Sativus_SamFiles/${kmer}.sat.bam : Sativus_SamFiles/${kmer}.sat.sam
 	samtools view -q 30 -b -T ${SatGenomeData_dir}/RSA_r1.0 $^ > $@
 
-Moghe_SamFiles/${kmer}.moghe.sorted: Moghe_SamFiles/${kmer}.moghe.bam
+Moghe_SamFiles/${kmer}.moghe.sorted : Moghe_SamFiles/${kmer}.moghe.bam
 	samtools sort $^ $@
 	samtools index $@.bam
 
@@ -35,30 +35,16 @@ Moghe_SamFiles/${kmer}.moghe.bam : Moghe_SamFiles/${kmer}.moghe.sam
 
 ChloroSat_SamFiles/${kmer}.chloro.sam :: ${RNAData_dir}/*.fastq.edit 
 	ls ChloroSat_SamFiles || mkdir ChloroSat_SamFiles
+	ls ${GSNAP_dir}/Sat_Chloro_${kmer} || gmap_build -d ${GSNAP_dir}/Sat_Chloro_${kmer} -k ${kmer} ${SatGenomeData_dir}/Rsativus_chloroplast.fa	
 	gsnap -d Sat_Chloro_${kmer} --force-single-end $^ -k ${kmer} -A sam -N 1 -O -n 1 -Q --nofails -o $@
-
-ChloroSat_SamFiles/${kmer}.chloro.sam :: ${GSNAP_dir}/Sat_Chloro_${kmer} 
 	
 Sativus_SamFiles/${kmer}.sat.sam :: ${RNAData_dir}/*.fastq.edit 
-	ls Sativus_SamFiles || mkdir Sativus_SamFiles 
+	ls Sativus_SamFiles || mkdir Sativus_SamFiles
+	ls ${GSNAP_dir}/Moghe_${kmer} || gmap_build -d ${GSNAP_dir}/Moghe_${kmer} -k ${kmer} ${SatGenomeData_dir}/RSA_r1.0	 
 	gsnap -d Sat_${kmer} --force-single-end $^ -k ${kmer} -A sam -N 1 -O -n 1 -Q --nofails -o $@
-
-Sativus_SamFiles/${kmer}.sat.sam :: ${GSNAP_dir}/Sat_${kmer}
 
 Moghe_SamFiles/${kmer}.moghe.sam :: ${RNAData_dir}/*.fastq.edit 
 	ls Moghe_SamFiles || mkdir Moghe_SamFiles
+	ls ${GSNAP_dir}/Moghe_${kmer} || gmap_build -d ${GSNAP_dir}/Moghe_${kmer} -k ${kmer} ${RapGenomeData_dir}/RrContigs.fa.fasta
 	gsnap -d Moghe_${kmer} --force-single-end $^ -k ${kmer} -A sam -N 1 -O -n 1 -Q --nofails -o $@
 
-Moghe_SamFiles/${kmer}.moghe.sam :: ${GSNAP_dir}/Moghe_${kmer} 
-
-	
-#Get Files
-
-${GSNAP_dir}/Sat_Chloro_${kmer} : 
-	gmap_build -d $@ -k ${kmer} ${SatGenomeData_dir}/Rsativus_chloroplast.fa	
-
-${GSNAP_dir}/Sat_${kmer} : 
-	gmap_build -d $@ -k ${kmer} ${SatGenomeData_dir}/RSA_r1.0	
-
-${GSNAP_dir}/Moghe_${kmer} : 
-	gmap_build -d $@ -k ${kmer} ${RapGenomeData_dir}/RrContigs.fa.fasta	
