564dae30b141d5d5560f7af6b84e29a11f130d50
Amanda Charbonneau <charbo24@msu.edu>
2015-10-16 18:50:08 -0400

    chloro is working in 1_mk others still need work

--- 1_RNAseq_HPCC.mk
+++ 1_RNAseq_HPCC.mk
@@ -6,62 +6,65 @@
 
 include scripts/config_1_HPC.mk 
 
-
 all : ChloroSat_SamFiles/bt2_chloro.sorted Sativus_SamFiles/bt2_sat.sorted Moghe_SamFiles/bt2_moghe.sorted
 	echo "From local machine run: make -f scripts/2_RNAseq_local.mk"
 
 # Convert New Files
 
 ChloroSat_SamFiles/bt2_chloro.sorted : ChloroSat_SamFiles/bt2_chloro.bam
-	module load SAMTools/1.2
 	samtools sort $^ $@
 	samtools index $@.bam
 
 ChloroSat_SamFiles/bt2_chloro.bam : ChloroSat_SamFiles/bt2_chloro.sam
-	module load SAMTools/1.2
-	samtools view -q 30 -b -T ${SatGenomeData_dir}/Rsativus_chloroplast.fa $^ > $@
+	qsub ViewSamtools.qsub -v genome="${SatGenomeData_dir}/Rsativus_chloroplast.fa",samfile="$^",outputfile="$@"
 
 Sativus_SamFiles/bt2_sat.sorted : Sativus_SamFiles/bt2_sat.bam
-	module load SAMTools/1.2
 	samtools sort $^ $@
 	samtools index $@.bam
 
 Sativus_SamFiles/bt2_sat.bam : Sativus_SamFiles/bt2_sat.sam
-	module load SAMTools/1.2
-	samtools view -q 30 -b -T ${SatGenomeData_dir}/RSA_r1.0 $^ > $@
+	qsub ViewSamtools.qsub -v genome="${SatGenomeData_dir}/RSA_r1.0",samfile="$^",outputfile="$@"
 
 Moghe_SamFiles/bt2_moghe.sorted : Moghe_SamFiles/bt2_moghe.bam
-	module load SAMTools/1.2
 	samtools sort $^ $@
 	samtools index $@.bam
 
 Moghe_SamFiles/bt2_moghe.bam : Moghe_SamFiles/bt2_moghe.sam
-	module load SAMTools/1.2
-	samtools view -q 30 -b -T ${RapGenomeData_dir}/RrContigs.fa.fasta $^ > $@
+	qsub ViewSamtools.qsub -v genome="${RapGenomeData_dir}/RrContigs.fa.fasta",samfile="$^",outputfile="$@"
+
+2015Sat_SamFiles/bt2_2015sat.sorted : 2015Sat_SamFiles/bt2_2015sat.bam
+	samtools sort $^ $@
+	samtools index $@.bam
+
+2015Sat_SamFiles/bt2_2015sat.bam : 2015Sat_SamFiles/bt2_2015sat.sam
+	qsub ViewSamtools.qsub -v genome="${2015SatGenomeData_dir}/rsg_all_v1.fasta",samfile="$^",outputfile="$@"
+
 
 # Mapping
 
-Moghe_SamFiles/bt2_moghe.sam : ${HPCRNAData_dir}/*.fastq.edit
-	module load bowtie2/2.2.6
+Moghe_SamFiles/bt2_moghe.sam : ${RNAData_dir}/*.fastq.edit
 	ls Moghe_SamFiles || mkdir Moghe_SamFiles
-	ls Bowtie_Indices/bt2_moghe*.bt2 || mkdir Bowtie_Indices && \
-		bowtie2-build RapGenomeData_dir/RrContigs.fa.fasta Bowtie_Indices/bt2_moghe
-	bowtie2 --local -D 15 -R 2 -N 0 -L 20 -i S,1,0.75 -x Bowtie_Indices/bt2_moghe -U $^ -S $@
+	ls Bowtie_Indices || mkdir Bowtie_Indices
+	ls Bowtie_Indices/bt2_moghe*.bt2 || qsub bt2_build.qsub -N BowtieMogheIndex -v genome="${RapGenomeData_dir}/RrContigs.fa.fasta",index="Bowtie_Indices/bt2_moghe"
+	qsub scripts/bt2_mapping.qsub -N "$@" -o Moghe_SamFiles/ -v genome="Bowtie_Indices/bt2_moghe",indiv="$^"
 
-ChloroSat_SamFiles/bt2_chloro.sam : ${HPCRNAData_dir}/*.fastq.edit
-	module load bowtie2/2.2.6
-	ls Chloro_SamFiles || mkdir Chloro_SamFiles
-	ls Bowtie_Indices/bt2_chloro*.bt2 || mkdir Bowtie_Indices && \
-		bowtie2-build SatGenomeData_dir/Rsativus_chloroplast.fa Bowtie_Indices/bt2_chloro
-	bowtie2 --local -D 15 -R 2 -N 0 -L 20 -i S,1,0.75 -x Bowtie_Indices/bt2_chloro -U $^ -S $@
-
-Sativus_SamFiles/bt2_sat.sam : ${HPCRNAData_dir}/*.fastq.edit
-	module load bowtie2/2.2.6
+ChloroSat_SamFiles/bt2_chloro.sam : ${RNAData_dir}/*.fastq.edit
+	ls ChloroSat_SamFiles || mkdir ChloroSat_SamFiles
+	ls Bowtie_Indices || mkdir Bowtie_Indices
+	ls Bowtie_Indices/bt2_chloro*.bt2 || qsub bt2_build.qsub -N BowtieChloroIndex -v genome="${SatGenomeData_dir}/Rsativus_chloroplast.fa",index="Bowtie_Indices/bt2_chloro"
+	qsub scripts/bt2_mapping.qsub -N "$@" -o ChloroSat_SamFiles/ -v genome="Bowtie_Indices/bt2_chloro",indiv="$^"
+	
+Sativus_SamFiles/bt2_sat.sam : ${RNAData_dir}/*.fastq.edit
 	ls sat_SamFiles || mkdir sat_SamFiles
-	ls Bowtie_Indices/bt2_sat*.bt2 || mkdir Bowtie_Indices && \
-		bowtie2-build SatGenomeData_dir/RSA_r1.0 Bowtie_Indices/bt2_sat
-	bowtie2 --local -D 15 -R 2 -N 0 -L 20 -i S,1,0.75 -x Bowtie_Indices/bt2_sat -U $^ -S $@
+	ls Bowtie_Indices || mkdir Bowtie_Indices
+	ls Bowtie_Indices/bt2_sat*.bt2 || qsub bt2_build.qsub -N BowtieSatIndex -v genome="${SatGenomeData_dir}/RSA_r1.0",index="Bowtie_Indices/bt2_sat"
+	qsub scripts/bt2_mapping.qsub -N "$@" -o Sativus_SamFiles/ -v genome="Bowtie_Indices/bt2_sat",indiv="$^"
 
+2015Sat_SamFiles/bt2_2015sat.sam : ${RNAData_dir}/*.fastq.edit
+	ls 2015Sat_SamFiles || mkdir 2015Sat_SamFiles
+	ls Bowtie_Indices || mkdir Bowtie_Indices
+	ls Bowtie_Indices/bt2_2015sat*.bt2 || qsub bt2_build.qsub -N 2015Sat_SamFiles -v genome="${2015SatGenomeData_dir}/rsg_all_v1.fasta",index="Bowtie_Indices/bt2_2015sat"
+	qsub scripts/bt2_mapping.qsub -N "$@" -o 2015Sat_SamFiles/ -v genome="Bowtie_Indices/bt2_2015sat",indiv="$^"
 
 metadata/SeqProductionSumm.xls :
 	cp ${metadata_dir}/SeqProductionSumm.xls metadata/SeqProductionSumm.xls
@@ -69,7 +72,7 @@ metadata/SeqProductionSumm.xls :
 fastqc/%.fastqc.html : %.fastq.edit
 	/opt/software/FastQC/0.11.3/fastqc $^ -o fastqc
 
-%.fastq.edit : 
+${RNAData_dir}/6_20081211_7_KL9.fastq.edit : 
 	bash ${Script_dir}/PrepRawData.sh
 	
 	
--- 2_RNAseq_local.mk
+++ 2_RNAseq_local.mk
@@ -5,12 +5,14 @@
 include scripts/config_2_Local.mk
 
 
-Aligned_reads.txt : ChloroSat_SamFiles/bt2_chloro.sorted ChloroSat_SamFiles/gSNAP_${kmer}_chloro.sorted Sativus_SamFiles/bt2_sat.sorted Sativus_SamFiles/gSNAP_${kmer}_sat.sorted Moghe_SamFiles/bt2_moghe.sorted Moghe_SamFiles/gSNAP_${kmer}_moghe.sorted
+Aligned_reads.txt : ChloroSat_SamFiles/bt2_chloro.sorted ChloroSat_SamFiles/gSNAP_${kmer}_chloro.sorted \
+Sativus_SamFiles/bt2_sat.sorted Sativus_SamFiles/gSNAP_${kmer}_sat.sorted \
+Moghe_SamFiles/bt2_moghe.sorted Moghe_SamFiles/gSNAP_${kmer}_moghe.sorted \
+2015Sat_SamFiles/bt2_2015sat.sorted 2015Sat_SamFiles/gSNAP_${kmer}_2015sat.sorted
 	for i in *SamFiles/*.sorted.bam; do samtools view -c $${i}; done > Aligned_reads.txt
 
-ChloroSat_SamFiles/bt2_chloro.sorted Sativus_SamFiles/bt2_sat.sorted Moghe_SamFiles/bt2_moghe.sorted : 
+ChloroSat_SamFiles/bt2_chloro.sorted Sativus_SamFiles/bt2_sat.sorted Moghe_SamFiles/bt2_moghe.sorted 2015Sat_SamFiles/bt2_2015sat.sorted : 
 	echo "make -f scripts/1_RNAseq_HPCC.mk"
-
 
 # Convert New Files
 
@@ -35,6 +37,13 @@ Moghe_SamFiles/gSNAP_${kmer}_moghe.sorted : Moghe_SamFiles/gSNAP_${kmer}_moghe.b
 Moghe_SamFiles/gSNAP_${kmer}_moghe.bam : Moghe_SamFiles/gSNAP_${kmer}_moghe.sam
 	samtools view -q 30 -b -T ${RapGenomeData_dir}/RrContigs.fa.fasta $^ > $@
 
+2015Sat_SamFiles/gSNAP_${kmer}_2015sat.sorted : 2015Sat_SamFiles/gSNAP_${kmer}_2015sat.bam
+	samtools sort $^ $@
+	samtools index $@.bam
+
+2015Sat_SamFiles/gSNAP_${kmer}_2015sat.bam : 2015Sat_SamFiles/gSNAP_${kmer}_2015sat.sam
+	samtools view -q 30 -b -T ${2015SatGenomeData_dir}/rsg_all_v1.fasta $^ > $@
+
 # Mapping
 
 ChloroSat_SamFiles/gSNAP_${kmer}_chloro.sam :: ${RNAData_dir}/*.fastq.edit 
@@ -51,4 +60,10 @@ Moghe_SamFiles/gSNAP_${kmer}_moghe.sam :: ${RNAData_dir}/*.fastq.edit
 	ls Moghe_SamFiles || mkdir Moghe_SamFiles
 	ls ${GSNAP_dir}/Moghe_${kmer} || gmap_build -d ${GSNAP_dir}/Moghe_${kmer} -k ${kmer} ${RapGenomeData_dir}/RrContigs.fa.fasta
 	gsnap -d Moghe_${kmer} --force-single-end $^ -k ${kmer} -A sam -N 1 -O -n 1 -Q --nofails -o $@
+
+2015Sat_SamFiles/gSNAP_${kmer}_2015sat.sam :: ${RNAData_dir}/*.fastq.edit 
+	ls 2015Sat_SamFiles || mkdir 2015Sat_SamFiles
+	ls ${GSNAP_dir}/2015Sat_${kmer} || gmap_build -d ${GSNAP_dir}/2015Sat_${kmer} -k ${kmer} ${2015SatGenomeData_dir}/rsg_all_v1.fasta 
+	gsnap -d 2015Sat_${kmer} --force-single-end $^ -k ${kmer} -A sam -N 1 -O -n 1 -Q --nofails -o $@
+
 
