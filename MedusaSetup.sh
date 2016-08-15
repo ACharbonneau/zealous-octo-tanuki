f9649c632a6a0bcdd852ef56a5bba3fa391a32fa
Amanda Charbonneau <charbo24@msu.edu>
2016-08-15 14:30:19 -0400

    changed base genome

--- MedusaSetup.sh
+++ MedusaSetup.sh
@@ -19,10 +19,10 @@ cp $Mitsui2015 Medusa3Assembly/ && mv Medusa3Assembly/rsg_all_v1.fasta Medusa3As
 cp $Kitashiba2014 Medusa3Assembly/drafts/ && mv Medusa3Assembly/drafts/Edit_RSA_r1.0 Medusa3Assembly/drafts/Kitashiba2014
 cp $Jeong2016 Medusa3Assembly/drafts/ && mv Medusa3Assembly/drafts/Rs_1.0.chromosomes.fix.fasta Medusa3Assembly/drafts/Jeong2016
 
-cp $Moghe2014 Medusa4Assembly/ && mv Medusa4Assembly/RrContigs.fa.fasta Medusa4Assembly/Moghe2014
+cp $Mitsui2015 Medusa4Assembly/ && mv Medusa4Assembly/rsg_all_v1.fasta Medusa4Assembly/Mitsui2015
 cp $Kitashiba2014 Medusa4Assembly/drafts/ && mv Medusa4Assembly/drafts/Edit_RSA_r1.0 Medusa4Assembly/drafts/Kitashiba2014
-cp $Mitsui2015 Medusa4Assembly/drafts/ && mv Medusa4Assembly/drafts/rsg_all_v1.fasta Medusa4Assembly/drafts/Mitsui2015
 cp $Jeong2016 Medusa4Assembly/drafts/ && mv Medusa4Assembly/drafts/Rs_1.0.chromosomes.fix.fasta Medusa4Assembly/drafts/Jeong2016
+cp $Moghe2014 Medusa4Assembly/drafts/ && mv Medusa4Assembly/drafts/RrContigs.fa.fasta Medusa4Assembly/drafts/Moghe2014
 
 
 
