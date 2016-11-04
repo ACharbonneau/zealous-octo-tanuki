e3901b6b4c550d8f370444993a766ad9ed1886d7
Amanda Charbonneau <charbo24@msu.edu>
2016-11-04 16:25:34 -0400

    rearranged files so you only start once

--- BUSCO.sh
+++ BUSCO.sh
@@ -6,4 +6,4 @@ qsub ../zealous-octo-tanuki/BUSCO.qsub -N Mitsui2015 -v genome=/mnt/research/rad
 
 qsub ../zealous-octo-tanuki/BUSCO.qsub -N Moghe2014 -v genome=/mnt/research/radishGenomics/AnalysisOfSequencingFiles/MoghePublished/RrContigs.fa.fasta
 
-qsub ../zealous-octo-tanuki/BUSCO.qsub -N AT2016 -v genome=TAIR10.fas
+qsub ../zealous-octo-tanuki/BUSCO.qsub -N AT2016 -v genome=/mnt/research/radishGenomics/PublicData/AT_TAIR10/TAIR10_chr_all.fas
