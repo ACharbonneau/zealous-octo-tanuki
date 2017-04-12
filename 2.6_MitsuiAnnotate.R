c44b946898359d41bc67a286d899ec58da555e3d
Amanda Charbonneau <charbo24@msu.edu>
2017-04-12 11:12:30 -0400

    Last step in Annotation

---
+++ 2.6_MitsuiAnnotate.R
@@ -0,0 +1,21 @@
+
+# Install function for packages
+packages<-function(x){
+  x<-as.character(match.call()[[2]])
+  if (!require(x,character.only=TRUE)){
+    install.packages(pkgs=x,repos="http://cran.r-project.org")
+    require(x,character.only=TRUE)
+  }
+}
+
+packages(dplyr)
+
+Genelist <- read.csv("../DEseqOutput/Mitsui2015_DE_BM20.csv")
+
+MitsuiAnn <- read.csv("../metadata/Mitsuiannotation/Annotation (tab).txt", sep = "\t")
+MitsuiAnn$gene_name <- sub(".t[0-9]", "", MitsuiAnn$Sequence.Name)
+
+
+AnnGenelist <- dplyr::left_join(Genelist, MitsuiAnn)
+
+write.csv(AnnGenelist, "../DEseqOutput/Annotated_Mitsui2015_DE_BM20.csv", row.names = F)
